78e51e0d1d63bb88bdfff1dc3153f8aa6db78d81
joonatanehlvest <joonatanehlvest123@gmail.com>
2024-07-31 16:40:41 +0300

    Worked on home page and navbar using bootstrap, changed the project structure to use blueprints

--- mmg/__init__.py
+++ mmg/__init__.py
@@ -4,7 +4,9 @@ from mmg import pages
 
 def create_app():
     app = Flask(__name__)
-
+    
+    app.secret_key = 'supersecretkey'  # Needed for flash messages
+    
     app.register_blueprint(pages.bp)
     return app
 
