c57ab89444a269eea416ed06d07a049ddcd56186
Julian Kaljuvee <kaljuvee@gmail.com>
2024-08-03 15:04:43 +0300

    Update __init__.py

--- mmg/__init__.py
+++ mmg/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
 from mmg import pages
+from admin import admin_bp
+from pages import bp as pages_bp
 
 def create_app():
     app = Flask(__name__)
     
     app.secret_key = 'supersecretkey'  # Needed for flash messages
-    
-    app.register_blueprint(pages.bp)
+    app.register_blueprint(pages_bp)
+    app.register_blueprint(admin_bp)
     return app
 
